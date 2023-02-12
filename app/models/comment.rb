9174a8b36a5f1280bc55f64ee966ab2b7540a5c9
Jasmin <ssongnari@gmail.com>
2023-02-12 14:02:58 -0500

    Seed data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :student
-    belongs_to :positivity_post
+    belongs_to :post
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
     has_secure_password
 
     has_many :comments
-    has_many: posts, through: :comments
+    has_many :posts, through: :comments
 
     validates :username, presence: true, uniqueness: true
 end
