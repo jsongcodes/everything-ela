2c83d76382f1900506ae9d1a3afc02a635bf14ff
Jasmin <ssongnari@gmail.com>
2023-02-12 15:58:16 -0500

    state for clicked

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -1,4 +1,9 @@
-const MainContainer = () => {
+import {useState} from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const MainContainer = ({postData, handleUpdateItem, student}) => {
+    const [clicked, setClicked] = useState(false);
+    
     return(
       <>
       MainContainer component
