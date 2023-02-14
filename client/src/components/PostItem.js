e2d130e3afdb9fa55f9c08e4759950ea980a238c
Jasmin <ssongnari@gmail.com>
2023-02-14 17:40:38 -0500

    Add fetch request

--- client/src/components/PostItem.js
+++ client/src/components/PostItem.js
@@ -1,12 +1,30 @@
-const PostItem = ({post, handleUpdateItem, clicked, setClicked}) => {
-    const {title, image, body, is_favorited: isFavorited} = post
+const PostItem = ({post, handleUpdateItem}) => {
+    const {id, title, image, body, is_favorited: isFavorited} = post
 
+    function handleClick(e) {
+        e.preventDefault();
+    
+        fetch(`/posts/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ is_favorited: !isFavorited }),
+        })
+          .then((r) => r.json())
+          .then((updatedItem) => handleUpdateItem(updatedItem))
+      }
+      
     return(
-      <>
-      {title}
-      {image}
-      {body}
-      </>
+        <div className="post-item">
+        <div>
+          <h2 className="post-card-title">{title}</h2>
+          <img src={image} className="postimage" alt="post"/>
+          {/* <h4 className="post-card-category">{body}</h4> */}
+          <p className="post-card-description">{body}...</p>
+        </div>
+
+      </div>
     )
   }
   
