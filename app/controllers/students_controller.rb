dc4596c66122da079901f6b7a7a72ce88faee598
Jasmin <ssongnari@gmail.com>
2023-02-14 17:29:22 -0500

    Added students controller routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,15 @@ class StudentsController < ApplicationController
         student = Student.find_by(id: session[:student_id])
         render json {errors: ["not authorized"]}, status: :unauthorized unless student
     end
+
+    def create
+        student = Student.create!(student_params)
+        render json: student, status: :created
+    end
+
+    private
+
+    def student_params
+        params.permit(:username, :password)
+    end
 end
