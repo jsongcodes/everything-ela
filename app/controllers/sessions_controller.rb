e798079e299cb09a94e26b9de514018cb9805839
Jasmin <ssongnari@gmail.com>
2023-02-14 17:28:30 -0500

    Add sessions controller routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+    def create
+        student = Student.find_by(username: params[:username])
+       
+        if student&.authenticate(params[:password])
+            session[:student_id] = student.id
+            render json: student, status: :created
+        else 
+            render json: {errors: ["Invalid username or password"] }, status: :unauthorized
+        end 
+    end 
+
+    def destroy
+        session.delete :student_id
+        head :no_content
+    end
+
 end
