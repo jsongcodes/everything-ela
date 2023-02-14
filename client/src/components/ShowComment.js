cd90a22ef8d4f714e142cda2b38ea3e5599e3d63
Jasmin <ssongnari@gmail.com>
2023-02-14 17:41:10 -0500

    Add fetch request

--- client/src/components/ShowComment.js
+++ client/src/components/ShowComment.js
@@ -1,9 +1,21 @@
+import {useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+
 const ShowComment = () => {
-    return(
-      <>
-      ShowComment component
-      </>
-    )
-  }
-  
-  export default ShowComment;
+    const [currentComment, setCurrentComment] = useState('')
+    const {id} = useParams();
+
+    useEffect(() => {
+        fetch(`/comments/${id}`)
+          .then((r) => r.json())
+          .then((c) => {
+            setCurrentComment(c)
+          });
+    }, [id]);
+
+  return (
+    <div className="show-comment">{currentComment.description}</div>
+  )
+}
+
+export default ShowComment;
