33949e7f39d87e937ae1cb3d858237c8bdd6c7f1
Jasmin <ssongnari@gmail.com>
2023-02-14 17:28:16 -0500

    Add post controller routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,24 @@ class PostsController < ApplicationController
         posts = Post.all
         render json: posts, status: :ok
     end
+
+    def show
+        post = find_post
+        render json: post, status: :ok
+    end
+
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
+    end
+
+    private
+    
+    def find_post
+        Post.find(params[:id])
+    end
+
+    def post_params
+        params.permit(:title, :image, :body)
+    end
 end
