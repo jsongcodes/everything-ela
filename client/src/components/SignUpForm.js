f73c0b49a0baa6bbcb3cace987f18727e712eb6f
Jasmin <ssongnari@gmail.com>
2023-02-14 17:41:27 -0500

    Add fetch request and formfield styling

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,8 +1,63 @@
-const SignUpForm = () => {
+import {useState} from 'react';
+import { Error, Input, FormField, Label } from "./styles";
+
+const SignUpForm = ({onLogin}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        fetch("/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password
+          }),
+        }).then((r) => {
+          if (r.ok) {
+            r.json().then((user) => onLogin(user));
+          } else {
+            r.json().then((err) => setErrors(err.errors));
+          }
+        });
+      }
+
     return(
-      <>
-      SignUpForm component
-      </>
+        <form onSubmit={handleSubmit}>
+        <FormField>
+          <Label htmlFor="username">Username</Label>
+          <Input
+            type="text"
+            id="username"
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </FormField>
+        <FormField>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+        </FormField>
+        <FormField>
+          <button type="submit">sign up</button>
+        </FormField>
+        <FormField>
+          {errors.map((err) => (
+            <Error key={err}>{err}</Error>
+          ))}
+        </FormField>
+      </form>
     )
   }
   
