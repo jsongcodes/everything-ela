0edecdd14105fcf1e8d1144508f04e3ca6abc3de
Jasmin <ssongnari@gmail.com>
2023-03-05 23:49:32 -0500

    delete renderPost

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -6,7 +6,7 @@ const PostForm = ({ addNewPost, studentId, posts }) => {
     title: "",
     image: "",
     body: "",
-    student_id: studentId
+    student_id: studentId,
   });
 
   const handleChange = (e) => {
@@ -22,66 +22,46 @@ const PostForm = ({ addNewPost, studentId, posts }) => {
     })
       .then((response) => response.json())
       .then((data) => {
-          addNewPost(data)
-          setInputForm({title: "", image: "", body: "", student_id: studentId
-        })
-        });
-  }
-
-  const renderPosts = posts.map((post) => {
-    return (
-      <div className="new-post-card" key={post.id}>
-        <Link to={`/posts/${post.id}`} className="post-card-title">
-          {post.title}
-        </Link>
-        <div>{post.body}</div>
-      </div>
-    );
-  });
+        addNewPost(data);
+        setInputForm({ title: "", image: "", body: "", student_id: studentId });
+      })
+  };
 
   return (
     <div className="main-container">
-    <form className="create-comment" onSubmit={(e) => handleSubmit(e)}>
-      <div>
-        <input
-          className="comment-input"
-          name="title"
-          type="text"
-          placeholder="Enter a title..."
-          value={inputForm.title}
-          onChange={handleChange}
-        ></input>
-      </div>
-      <div>
-        <input
-          className="comment-input"
-          name="image"
-          type="text"
-          placeholder="Enter a image url..."
-          value={inputForm.image}
-          onChange={handleChange}
-        ></input>
-      </div>
-      <div>
-        <input
-          className="comment-input"
-          name="body"
-          type="text"
-          placeholder="Enter a body..."
-          value={inputForm.body}
-          onChange={handleChange}
-        ></input>
-      </div>
-      <input className="submit-button" type="submit" value="Post" />
-    </form>
-
-    <div className="new-post-list">
-      <h1 className="accountquestion">new posts</h1>
-      <div className="new-post-card">
-        <div className="post-card-description">{renderPosts}</div>
-      </div>
-    </div>
-    
+      <form className="create-comment" onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <input
+            className="comment-input"
+            name="title"
+            type="text"
+            placeholder="Enter a title..."
+            value={inputForm.title}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <input
+            className="comment-input"
+            name="image"
+            type="text"
+            placeholder="Enter a image url..."
+            value={inputForm.image}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <input
+            className="comment-input"
+            name="body"
+            type="text"
+            placeholder="Enter a body..."
+            value={inputForm.body}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <input className="submit-button" type="submit" value="Post" />
+      </form>
     </div>
   );
 };
